2be53d56a437152a50d054cbda56f7688e231ec2
manchuriqbal <QYWfiHfGiRd4L9n>
2024-04-29 01:47:40 +0600

    complete pagination

--- admin/users.php
+++ admin/users.php
@@ -15,7 +15,16 @@
 
                 <?php
                   include "config.php";
-                  $query = "SELECT * FROM user ORDER BY id DESC";
+
+                  if (isset($_GET['page'])) {
+                    $page_number = $_GET['page'];
+                  } else {
+                    $page_number = 1;
+                  }
+
+                  $limit =3;
+                  $offset = ($page_number - 1) * $limit;
+                  $query = "SELECT * FROM user ORDER BY id DESC LIMIT {$offset}, {$limit}";
                   $result = mysqli_query($connection, $query);
                   $count = mysqli_num_rows($result);
 
@@ -54,20 +63,55 @@
                         
                         ?>
                         </td>
-                        <td class='edit'><a href='update-user.php?id='><i class='fa fa-edit'></i></a></td>
+                        <td class='edit'><a href="update-user.php?id=<?php echo $row['id'] ?>"><i class='fa fa-edit'></i></a></td>
                         
 
-                        <td class='delete'><a onclick="return confirm('Are You Sure?')" href='delete-user.php?id='><i class='fa fa-trash-o'></i></a></td>
+                        <td class='delete'><a onclick="return confirm('Are You Sure?')" href="delete-user.php?id=<?php echo $row['id'] ?>"><i class='fa fa-trash-o'></i></a></td>
                     </tr>
 
+
+                    <?php 
+                        }
+                    ?>
+                    </tbody>
+                </table>
+
               <?php 
+                  }
+
+                  include "config.php";
+                  $query1 = "SELECT * FROM user";
+                  $result1 = mysqli_query($connection, $query1);
+                  
+                  if (mysqli_num_rows($result1)) {
+                    $total_user = mysqli_num_rows($result1);
+                    $total_page = ceil($total_user/$limit);
+                      echo '<ul class="pagination">';
+                      if ($page_number>1) {
+                        echo '<li><a href="users.php?page='.($page_number-1).'">prev</a></li>';
+                      }
+                    for ($i=1; $i <=$total_page ; $i++) { 
+                      if ($page_number==$i) {
+                        $active = "active";
+                      } else {
+                        $active = "";
+                      }
+
+                      echo '<li class="'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
                     }
+
+                    if ($total_page>$page_number) {
+                    echo '<li><a href="users.php?page='.($page_number+1).'">next</a></li>';
+                    }
+                    echo '</ul>';
+
                   }
               ?>
 
 
 
-                      <!-- <li class="active"><a>1</a></li> -->
+                      <!-- <li class="active"><a href="">1</a></li> -->
+
                     
 
 
