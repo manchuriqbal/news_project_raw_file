297a7f466a41422a25a6e826b477ce9551d2c18c
manchuriqbal <QYWfiHfGiRd4L9n>
2024-05-08 12:06:01 +0600

    prevent direct access to subfolder

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -4,6 +4,12 @@
     $post_id = $_GET['id'];
     $category_id = $_GET['category_id'];
 
+    $query1 = "SELECT * FROM post WHERE post_id = '{$post_id}'";
+    $result1 = mysqli_query($connection, $query1);
+    $row = mysqli_fetch_assoc($result1);
+
+    unlink("upload/".$row['post_img']);
+
     $query = "DELETE FROM post WHERE post_id = '{$post_id}' ;";
     $query .= "UPDATE category SET post = post - 1 WHERE category_id = '{$category_id}'";
 
@@ -12,5 +18,4 @@
     if ($result) {
         header('location: post.php');
     }
-
 ?>
