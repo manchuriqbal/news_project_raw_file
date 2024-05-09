5c70a47a56347abb46fda2705a54dd08f6ece7d0
manchuriqbal <QYWfiHfGiRd4L9n>
2024-05-09 12:42:02 +0600

    complete search page. and finally my website are complete

--- admin/post.php
+++ admin/post.php
@@ -11,7 +11,7 @@
             <div class="col-md-12">
                 <?php
                 include "config.php";
-                $limit = 3;
+                $limit = 5;
 
                 if (isset($_GET['page'])) {
                     $page_number = $_GET['page'];
--- sidebar.php
+++ sidebar.php
@@ -15,91 +15,45 @@
     <!-- recent posts box -->
     <div class="recent-post-container">
         <h4>Recent Posts</h4>
+
+<?php 
+
+include "admin/config.php";
+$limit = 8;
+$query = "SELECT post.post_id, post.post_title, post.post_date, post.post_img, category.category_id, category.category_name FROM post 
+LEFT JOIN category ON post.category = category.category_id ORDER BY post_id DESC LIMIT {$limit} ";
+$result = mysqli_query($connection, $query);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        
+
+
+?>
         <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post-format.jpg" alt=""/>
+            <a class="post-img"href="single.php?id=<?php echo $row['post_id'] ?>">
+                <img  src="admin/upload/<?php echo $row['post_img'] ?>" alt=""/>
             </a>
             <div class="post-content">
-                <h5><a href="single.php">Web Development</a></h5>
+                <h5><a href="single.php?id=<?php echo $row['post_id'] ?>"><?php echo $row['post_title'] ?></a></h5>
                 <span>
                     <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Html</a>
+                    <a href='category.php?category_id=<?php echo $row['category_id']?>'><?php echo $row['category_name'] ?></a>
                 </span>
                 <span>
                     <i class="fa fa-calendar" aria-hidden="true"></i>
-                    19 July, 2020
+                   <?php  echo $row['post_date'] ?>
                 </span>
-                <a class="read-more" href="single.php">read more</a>
+                <a class="read-more" href="single.php?id=<?php echo $row['post_id'] ?>">read more</a>
             </div>
         </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post_1.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Apps Development</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Html</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    19 July, 2020
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post-format.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Web Development</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Html</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    19 July, 2020
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post_1.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Apps Development</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Html</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    19 July, 2020
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
-        <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post-format.jpg" alt=""/>
-            </a>
-            <div class="post-content">
-                <h5><a href="single.php">Apps Development</a></h5>
-                <span>
-                    <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Html</a>
-                </span>
-                <span>
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    19 July, 2020
-                </span>
-                <a class="read-more" href="single.php">read more</a>
-            </div>
-        </div>
+
+        <?php 
+  
+    }
+}
+
+?>
+
     </div>
     <!-- /recent posts box -->
 </div>
